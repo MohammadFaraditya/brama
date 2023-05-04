61ab62181cd2540568454d1ca2ae6f2ec1e1ffb2
MohammadFaraditya <125716350+MohammadFaraditya@users.noreply.github.com>
2023-05-04 10:16:48 +0700

    Add files via upload

---
+++ Project Brama/Script/menu.js
@@ -0,0 +1,25 @@
+
+// Function menampilkan sidebar
+document.querySelector('#menu').onclick = () => {
+
+    const displayNavbar = document.querySelector('#navbar');
+    const menuClose = document.querySelector('#close');
+    const menu = document.querySelector('#menu')
+
+    displayNavbar.style.right = "0";
+    displayNavbar.style.transition = "0.7s"
+    menuClose.style.display = "block";
+    menu.style.display = "none";
+}
+
+
+// Function untuk menutup sidebar
+document.querySelector('#close').onclick = () => {
+    const displayNavbar = document.querySelector('#navbar');
+    const menuClose = document.querySelector('#close');
+    const menu = document.querySelector('#menu')
+
+    displayNavbar.style.right = "-100%";
+    menuClose.style.display = "none";
+    menu.style.display = "block";
+}
